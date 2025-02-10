4d63fb72b36ffc73716f41ab9100b33695690809
shatadalsamui <143749872+shatadalsamui@users.noreply.github.com>
2025-02-10 09:00:00 +0530

    Fixed

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -1,22 +1,16 @@
 "use server"
-
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
-import prisma from "@repo/db/client"
-
+import prisma from "@repo/db/client";
 
 export async function p2pTransfer(to: string, amount: number) {
-    // extract the user from the session and do the necessary checks
     const session = await getServerSession(authOptions);
-
     const from = session?.user?.id;
-
     if (!from) {
         return {
-            message: "Error while Sending"
+            message: "Error while sending"
         }
     }
-
     const toUser = await prisma.user.findFirst({
         where: {
             number: to
@@ -28,27 +22,22 @@ export async function p2pTransfer(to: string, amount: number) {
             message: "User not found"
         }
     }
-    //prisma specific method of updating database which is atomic database transaction - either all txns happen or none do 
-    //same reason tx is used - tx is special transaction-scoped prisma client all tx txns are a part of the atomic txns
     await prisma.$transaction(async (tx) => {
         const fromBalance = await tx.balance.findUnique({
-            where: {
-                userId: Number(from)
-            }
-        });
-        if (!fromBalance || fromBalance.amount < amount) {
-            throw new Error("Insuffiecient Funds");
-        }
-
-        await tx.balance.update({
             where: { userId: Number(from) },
-            data: { amount: { decrement: amount } }
-        });
+          });
+          if (!fromBalance || fromBalance.amount < amount) {
+            throw new Error('Insufficient funds');
+          }
 
-        await tx.balance.update({
+          await tx.balance.update({
+            where: { userId: Number(from) },
+            data: { amount: { decrement: amount } },
+          });
+
+          await tx.balance.update({
             where: { userId: toUser.id },
             data: { amount: { increment: amount } },
-        });
-
+          });
     });
 }
--- apps/user-app/components/SendCard.tsx
+++ apps/user-app/components/SendCard.tsx
@@ -1,14 +1,12 @@
 "use client"
-
+import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
 import { Center } from "@repo/ui/center";
 import { TextInput } from "@repo/ui/textinput";
 import { useState } from "react";
-import { Button } from "@repo/ui/button";
 import { p2pTransfer } from "../app/lib/actions/p2pTransfer";
 
 export function SendCard() {
-
     const [number, setNumber] = useState("");
     const [amount, setAmount] = useState("");
 
@@ -16,11 +14,10 @@ export function SendCard() {
         <Center>
             <Card title="Send to Phone Number">
                 <div className="min-w-72 pt-2">
-                    <TextInput placeholder={"Number"} label={"Number"} onChange={(value) => {
-                        setAmount(value)
+                    <TextInput placeholder={"Number"} label="Number" onChange={(value) => {
+                        setNumber(value)
                     }} />
-
-                    <TextInput placeholder={"Amount"} label={"Amount"} onChange={(value) => {
+                    <TextInput placeholder={"Amount"} label="Amount" onChange={(value) => {
                         setAmount(value)
                     }} />
                     <div className="pt-4 flex justify-center">
