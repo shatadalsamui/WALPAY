50bd78d73f8eb2b0f612d819e4ebdebac2cd6047
shatadalsamui <143749872+shatadalsamui@users.noreply.github.com>
2025-02-10 09:00:00 +0530

    Updated

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -15,11 +15,22 @@ export const Appbar = ({
     onSignout
 }: AppbarProps) => {
     return <div className="flex justify-between border-b px-4 border-slate-300">
-        <div className="text-4xl flex flex-col text-[#333333] font-bold justify-center">
-            WALPAY
+
+        <div className="flex items-center gap-2">
+            <WALPAYIcon />
+            <div className="text-4xl text-[#333333] font-bold">
+                WALPAY
+            </div>
         </div>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
         </div>
     </div>
+}
+
+function WALPAYIcon() {
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-10">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 8.25H9m6 3H9m3 6-3-3h1.5a3 3 0 1 0 0-6M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+
 }
