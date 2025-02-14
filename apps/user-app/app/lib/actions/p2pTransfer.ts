05ad1dfc2a8c1eb6e7eb2e85b98b6a2ddccd9995
shatadalsamui <143749872+shatadalsamui@users.noreply.github.com>
2025-02-14 09:00:00 +0530

    .

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -46,7 +46,7 @@ export async function p2pTransfer(to: string, amount: number) {
 
         await tx.p2pTransfer.create({
             data: {
-                fromUserId: from,
+                fromUserId: Number(from),
                 toUserId: toUser.id,
                 amount,
                 timestamp: new Date()
