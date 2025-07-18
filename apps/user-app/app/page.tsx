7a093b24d3a4225dc6cbd1c0bfaf1a2b1982f459
shatadalsamui <143749872+shatadalsamui@users.noreply.github.com>
2025-07-19 00:25:23 +0530

    Updated

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
 export default async function Page() {
+ 
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
